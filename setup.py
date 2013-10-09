b45c6ccef751f42fabbea01558a200ae11623ded
Fredrik Appelros <fredrik.appelros@proceranetworks.com>
2013-10-09 10:10:42 +0200

    Numpy include
    Made sure that setuptools finds numpy headers.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy as np
+
 setup(name="align",
       version='0.1',
       description='Global and local sequence alignment implemented in C for speed.',
@@ -11,5 +13,5 @@ setup(name="align",
       py_modules=['align'],
       install_requires=['numpy', 'cython'],
       cmdclass = {'build_ext': build_ext},
-      ext_modules = [Extension('align', ['align.pyx', 'calign.c'])],
+      ext_modules = [Extension('align', ['align.pyx', 'calign.c'], include_dirs=[np.get_include()])],
 )
